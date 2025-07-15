e65f7b1dd88a4f9a1fccbaabb8ea98d19b2cff1f
fiqhinre <fiqhinadya@gmail.com>
2025-07-16 00:42:00 +0700

    Update responsive looks

--- main.js
+++ main.js
@@ -4,3 +4,8 @@
       window.location.href = "next.html"; // Ganti dengan file tujuanmu
     };
   });
+
+  function toggleMenu() {
+  const menu = document.getElementById('nav-menu');
+  menu.classList.toggle('show');
+}
