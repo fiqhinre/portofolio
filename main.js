24826c080b74238ad93beb97fb49a73a5141141e
fiqhinre <fiqhinadya@gmail.com>
2025-07-15 23:54:23 +0700

    update tombol dan js

---
+++ main.js
@@ -0,0 +1,6 @@
+
+  document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("go-next").onclick = function () {
+      window.location.href = "next.html"; // Ganti dengan file tujuanmu
+    };
+  });
